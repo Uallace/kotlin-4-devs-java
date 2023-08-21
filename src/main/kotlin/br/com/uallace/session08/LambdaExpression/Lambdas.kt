38bc2562d2e60adbf90dff95918e3b210040bd99
uallace-gomes-silva <uallace.gomes.silva@ibm.com>
2023-08-21 20:29:31 -0300

    Trabalhando com lambda

---
+++ src/main/kotlin/br/com/uallace/session08/LambdaExpression/Lambdas.kt
@@ -0,0 +1,13 @@
+package br.com.uallace.session08.LambdaExpression
+
+fun main(){
+
+    val students = getStudents()
+
+    val combos = students.map { a -> "${a.name} : ${a.age}" }
+    println("Combos: $combos")
+
+    println("The oldest student is: ${students.maxByOrNull { it.age }}")
+    println("Student with longest name is: ${students.filter { it.name.length > 5 }}")
+
+}
---
+++ src/main/kotlin/br/com/uallace/session08/LambdaExpression/Student.kt
@@ -0,0 +1,13 @@
+package br.com.uallace.session08.LambdaExpression
+
+data class Student(val name: String, val age: Int)
+
+fun getStudents(): List<Student> {
+    return listOf(
+        Student("Flavio", 19),
+        Student("Rebeca", 20),
+        Student("Maria", 23),
+        Student("Walter", 18),
+        Student("Sergio", 21)
+    )
+}
